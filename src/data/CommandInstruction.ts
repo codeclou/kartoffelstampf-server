499fc6e9d55650b07154b7fd55229737a97a3feb
Bernhard Grünewaldt <github@gruenewaldt.net>
2017-03-18 19:22:57 +0100

    websocket command service

--- src/data/CommandInstruction.ts
+++ src/data/CommandInstruction.ts
@@ -1,3 +1,8 @@
+/*!
+ * Licensed under MIT License
+ * Copyright (c) 2017 Bernhard Grünewaldt
+ * https://github.com/codeclou/kartoffelstampf-server
+ */
 class CommandInstruction {
 
   public command: string = 'optipng';
